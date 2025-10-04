efb0e5462e80f24e87cfddd08e39c81a797acba1
Emiel Estiévenart <emiel-perijah@hotmail.com>
2025-10-04 16:20:49 +0200

    Apply formatting and small fixes: update examples, fix socket type, add launch configs

--- examples/sfinae/robot_mover.hpp
+++ examples/sfinae/robot_mover.hpp
@@ -6,30 +6,36 @@ namespace eestv
 {
 
 // Trait: has a member function `move()` callable on an lvalue T
-template <typename, typename = void> struct has_member_move : std::false_type
+template <typename, typename = void>
+struct has_member_move : std::false_type
 {
 };
 
-template <typename T> struct has_member_move<T, std::void_t<decltype(std::declval<T&>().move())> > : std::true_type
+template <typename T>
+struct has_member_move<T, std::void_t<decltype(std::declval<T&>().move())> > : std::true_type
 {
 };
 
 // Trait: has a free function `move(T&)` found by ADL (unqualified call)
-template <typename, typename = void> struct has_adl_move : std::false_type
+template <typename, typename = void>
+struct has_adl_move : std::false_type
 {
 };
 
-template <typename T> struct has_adl_move<T, std::void_t<decltype(move(std::declval<T&>()))> > : std::true_type
+template <typename T>
+struct has_adl_move<T, std::void_t<decltype(move(std::declval<T&>()))> > : std::true_type
 {
 };
 
 // Helper to produce better static_assert messages
-template <typename T> struct always_false : std::false_type
+template <typename T>
+struct always_false : std::false_type
 {
 };
 
 // Main dispatcher: prefer member, otherwise ADL free function, otherwise static assert
-template <typename RobotType> void move_the_robot(RobotType& robot)
+template <typename RobotType>
+void move_the_robot(RobotType& robot)
 {
     if constexpr (has_member_move<RobotType>::value)
     {
--- examples/templates/templated_executable2.hpp
+++ examples/templates/templated_executable2.hpp
@@ -9,8 +9,9 @@
  * This class demonstrates another approach to templated executables,
  * with different execution logic and functionality.
  */
-template<typename T>
-class TemplatedExecutable2 {
+template <typename T>
+class TemplatedExecutable2
+{
 private:
     T value;
     T multiplier;
@@ -29,15 +30,18 @@ public:
      * The parameter names clearly indicate their purpose to avoid confusion.
      */
     // NOLINTNEXTLINE(readability-identifier-length,bugprone-easily-swappable-parameters)
-    TemplatedExecutable2(T initial_value, T multiplication_factor, const std::string& name = "Executable2") 
-        : value(initial_value), multiplier(multiplication_factor), identifier(name) {}
+    TemplatedExecutable2(T initial_value, T multiplication_factor, const std::string& name = "Executable2")
+        : value(initial_value), multiplier(multiplication_factor), identifier(name)
+    {
+    }
 
     /**
      * @brief Execute the operation - multiplies value by multiplier
      * 
      * @return T The result of value * multiplier
      */
-    T execute() {
+    T execute()
+    {
         std::cout << identifier << " executing: " << value << " * " << multiplier;
         T result = value * multiplier;
         std::cout << " = " << result << '\n';
@@ -50,9 +54,9 @@ public:
      * @param additional Additional value to add to the result
      * @return T The result of (value * multiplier) + additional
      */
-    T executeWithAddition(T additional) {
-        std::cout << identifier << " executing with addition: (" << value 
-                  << " * " << multiplier << ") + " << additional;
+    T executeWithAddition(T additional)
+    {
+        std::cout << identifier << " executing with addition: (" << value << " * " << multiplier << ") + " << additional;
         T result = (value * multiplier) + additional;
         std::cout << " = " << result << '\n';
         return result;
@@ -63,48 +67,33 @@ public:
      * 
      * @return T The current value
      */
-    T getValue() const {
-        return value;
-    }
+    T getValue() const { return value; }
 
     /**
      * @brief Get the multiplier
      * 
      * @return T The multiplier
      */
-    T getMultiplier() const {
-        return multiplier;
-    }
+    T getMultiplier() const { return multiplier; }
 
     /**
      * @brief Set a new value
      * 
      * @param newValue The new value to set
      */
-    void setValue(const T& newValue) {
-        value = newValue;
-    }
+    void setValue(const T& newValue) { value = newValue; }
 
     /**
      * @brief Set a new multiplier
      * 
      * @param newMultiplier The new multiplier to set
      */
-    void setMultiplier(const T& newMultiplier) {
-        multiplier = newMultiplier;
-    }
+    void setMultiplier(const T& newMultiplier) { multiplier = newMultiplier; }
 
     /**
      * @brief Get the identifier
      * 
      * @return std::string The identifier
      */
-    std::string getIdentifier() const {
-        return identifier;
-    }
+    std::string getIdentifier() const { return identifier; }
 };
-
-// Convenience type aliases for common uses
-using IntExecutable2 = TemplatedExecutable2<int>;
-using DoubleExecutable2 = TemplatedExecutable2<double>;
-using FloatExecutable2 = TemplatedExecutable2<float>;
